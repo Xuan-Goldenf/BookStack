4e4ec7cf6deb9cca42d544e1cdc5b9fa1b9d1c3e
Xuan Yang <xuan.yang@goldenf.co>
2022-08-15 19:08:32 +0800

    fix font-size

--- resources/views/exports/parts/book-contents-menu.blade.php
+++ resources/views/exports/parts/book-contents-menu.blade.php
@@ -1,7 +1,7 @@
 @if(count($children) > 0)
     <ul class="contents">
         @foreach($children as $bookChild)
-            <li><a href="#{{$bookChild->getType()}}-{{$bookChild->id}}">{{ $bookChild->name }}</a></li>
+            <li><a href="#{{$bookChild->getType()}}-{{$bookChild->id}}" style="font-size: 20px;">{{ $bookChild->name }}</a></li>
             @if($bookChild->isA('chapter') && count($bookChild->visible_pages) > 0)
                 @include('exports.parts.chapter-contents-menu', ['pages' => $bookChild->visible_pages])
             @endif
--- resources/views/exports/parts/chapter-contents-menu.blade.php
+++ resources/views/exports/parts/chapter-contents-menu.blade.php
@@ -1,7 +1,7 @@
 @if (count($pages) > 0)
-        <ul class="contents">
+        <ul class="contents" style="font-size: 20px;">
             @foreach($pages as $page)
-                <li><a href="#page-{{$page->id}}">{{ $page->name }}</a></li>
+                <li><a href="#page-{{$page->id}}" style="font-size: 20px;">{{ $page->name }}</a></li>
             @endforeach
         </ul>
 @endif
